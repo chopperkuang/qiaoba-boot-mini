364fa350b7f09995e1d243120d1abffdbc7840a3
kuang <chopperkuang@gmail.com>
2024-09-02 10:53:02 +0800

    添加项目

---
+++ yudao-server/src/main/java/cn/iocoder/yudao/server/YudaoServerApplication.java
@@ -0,0 +1,21 @@
+package cn.iocoder.yudao.server;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目的启动类
+ *
+ * @author 芋道源码
+ */
+@SuppressWarnings("SpringComponentScan") // 忽略 IDEA 无法识别 ${yudao.info.base-package}
+@SpringBootApplication(scanBasePackages = {"${qiaoba.info.base-package}.module", "${yudao.info.base-package}.server", "${yudao.info.base-package}.module"})
+public class YudaoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YudaoServerApplication.class, args);
+    }
+
+}
